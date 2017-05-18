e3b386b3fa154fd648ad59384cd671d13d08e149
Jeff Charlesworth <jcharlesworth02@gmail.com>
2017-05-18 14:32:14 -0700

    CRUD added, views updated

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,54 @@
+class WikisController < ApplicationController
+  def new
+    @wiki = Wiki.new
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def create
+    @wiki = Wiki.new({title: params['wiki']['title'], body: params['wiki']['body']})
+    @wiki.user = current_user
+
+    if @wiki.save
+      flash[:notice] = "Wiki created"
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "Error creating wiki, please try again"
+    end
+  end
+
+  def index
+    @wikis = Wiki.where(user_id: current_user.id)
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.save
+      @wiki.update_attributes({ title: params[:wiki][:title], body: params[:wiki][:body] })
+      flash.now[:notice] = "Wiki updated."
+      render :show
+    else
+      flash.now[:alert] = "Unable to update the wiki, please try again."
+      render :show
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" deleted."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "Unable to delete Wiki, please try again later."
+      render :show
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :wikis
   devise_for :users
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
